d379fdb240f30189c867820d1a4de8f4a5912698
Prince_Sabu <psabu@merpsystems.com>
2025-03-30 20:02:42 +0530

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,64 @@
+import "./style.css";
+
+const login = document.getElementById("login");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const home = document.getElementById("homePage");
+const transactions = document.getElementById("transactionsPage");
+const budget = document.getElementById("budgetPage");
+const subscription = document.getElementById("subscriptionPage");
+const goal = document.getElementById("goalPage");
+const report = document.getElementById("reportPage");
+
+if (login) {
+  login.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (username.value == "admin" && password.value == "Password@123") {
+      window.location.href = "/src/Other/home.html";
+    } else {
+      alert("Invalid username or password");
+    }
+  });
+}
+
+if (home) {
+  home.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "/src/Other/home.html";
+  });
+}
+
+if (transactions) {
+  transactions.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "/src/Other/Transaction.html";
+  });
+}
+
+if (budget) {
+  budget.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "/src/Other/Budget.html";
+  });
+}
+
+if (subscription) {
+  subscription.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "/src/Other/Subscription.html";
+  });
+}
+
+if (goal) {
+  goal.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "/src/Other/goal.html";
+  });
+}
+
+if (report) {
+  report.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "/src/Other/report.html";
+  });
+}
---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [tailwindcss()],
+  server: {
+    host: "0.0.0.0",
+    port: 5555,
+    strictPort: true,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        budget: "src/Other/Budget.html",
+        goal: "src/Other/goal.html",
+        home: "src/Other/home.html",
+        report: "src/Other/report.html",
+        subscription: "src/Other/Subscription.html",
+        transaction: "src/Other/Transaction.html",
+      },
+    },
+  },
+});
