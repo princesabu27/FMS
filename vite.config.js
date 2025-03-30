04b4dd115906620bf0e28b9678aff4b1d3c20939
Prince_Sabu <psabu@merpsystems.com>
2025-03-30 20:16:16 +0530

    Second commit, change the deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "FMS",
   plugins: [tailwindcss()],
   server: {
     host: "0.0.0.0",
